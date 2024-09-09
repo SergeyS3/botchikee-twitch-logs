20e2b2211280cade0ceb63f3fb320aafccf262c8
Sergey Semichastnov <Sergey--S@yandex.ru>
2024-09-09 16:50:47 +0300

    backend refactor

--- index.js
+++ index.js
@@ -1,51 +1,33 @@
 const http = require('http')
 const fs = require('fs')
-const { globSync } = require('glob')
 
-const port = process.env.PORT
-const logsPath = process.env.LOGS_LOCAL_PATH
+const { PORT, LOGS_LOCAL_PATH, LOGS_EXTERNAL_PATH } = process.env
+const htmlTemplate = fs.readFileSync(__dirname + '/src/index.html').toString()
 
 const server = http.createServer((req, res) => {
+  const writeResponse = (status, text, headers) => res.writeHead(status, headers).end(text)
+
   try {
-    const dates = globSync(`${logsPath}/**/*.log`)
-      .sort()
-      .reduce((acc, path) => {
-        const [channel, date] = path
-          .replace('.log', '')
-          .replace(`${logsPath}/`, '')
-          .split('/')
-
-        if (!acc[channel])
-          acc[channel] = [date]
-        else
-          acc[channel].push(date)
-
-        return acc
-      }, {})
-
     const { searchParams } = new URL(req.url, 'x://x')
     const channel = searchParams.get('channel')
+    const folder = `${LOGS_LOCAL_PATH}/${channel}`
 
-    if (!channel || !dates[channel]) {
-      res.writeHead(404)
-      res.end('404 Not found')
-      return
-    }
+    if (!channel || !/^[a-zA-Z0-9_]+$/.test(channel) || !fs.existsSync(folder))
+      return writeResponse(404, '404 Not found')
 
-    const html = fs.readFileSync(__dirname + '/src/index.html').toString()
-      .replace('#DATA#', JSON.stringify(dates[channel]))
-      .replace('#LOGS_EXTERNAL_PATH#', `${process.env.LOGS_EXTERNAL_PATH}/${channel}`)
+    const dates = fs.readdirSync(folder).map(f => f.slice(0, -4))
+    const html = htmlTemplate
+      .replace('#DATA#', JSON.stringify(dates))
+      .replace('#LOGS_EXTERNAL_PATH#', `${LOGS_EXTERNAL_PATH}/${channel}`)
 
-    res.writeHead(200, { 'Content-type': 'text/html' })
-    res.end(html)
+    writeResponse(200, html, { 'Content-type': 'text/html' })
   } catch (e) {
     console.error(e)
 
-    res.writeHead(500)
-    res.end('500 Internal Server Error')
+    writeResponse(500, '500 Internal Server Error')
   }
 })
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
 })
